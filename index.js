0cad3b0f4b89c39869da64b10b7006816369fea4
RegisLewis <regisaslewis@gmail.com>
2023-03-15 17:52:29 -0700

    class extension lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+class Polygon {
+  constructor(array) {
+    this.array = array;
+  }
+  get countSides() {
+    return this.array.length;
+  }
+  get perimeter() {
+    return this.array.reduce((a, b) => a + b, 0)
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.array[0] + this.array[1] > this.array[2] && 
+        this.array[0] + this.array[2] > this.array[1] && 
+        this.array[1] + this.array[2] > this.array[0]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (this.array[0] === this.array[1] &&
+        this.array[0] === this.array[2] &&
+        this.array[0] == this.array[3]) {
+        return true;
+    } else {
+      return false;
+    }
+  }
+  get area() {
+    return this.array[0] * this.array[0];
+  }
+}
